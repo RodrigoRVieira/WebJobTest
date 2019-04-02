b5e14fb1443ea45d76b06245e1449d78c672ff51
Rodrigo Riccitelli Vieira <rrvieira@br.atento.com>
2019-04-02 11:11:52 -0300

    Add Sample

---
+++ WebJobTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WebJobTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
